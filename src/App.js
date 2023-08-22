a7b088bcd52416defebf6177410fb8e459e726cf
Joseph Howard <succinction@gmail.com>
2023-08-22 12:50:34 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import logo from './mac-icon-3308.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="title">
+          Portland&nbsp;Mac&nbsp;Repair
+          <br />
+          (503) 875-6345
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="subtitle"><i>Let me help you today...</i></p>
+        <p className="subtitle">
+          Computer Repair Service
+        </p>
+        <p className="services">
+          Tablet&nbsp;repair &nbsp;  &nbsp;
+          Data&nbsp;Retrieval &nbsp;  &nbsp;
+          Hard&nbsp;Drive&nbsp;Replacement &nbsp;  &nbsp;
+          Liquid&nbsp;Damage &nbsp;  &nbsp;
+          Mac&nbsp;Repair &nbsp;  &nbsp;
+          Macbook&nbsp;Pro &nbsp;  &nbsp;
+          New&nbsp;Computer &nbsp;  &nbsp;
+          Screen&nbsp;Replacement &nbsp;  &nbsp;
+          Virus&nbsp;Removal &nbsp;  &nbsp;
+          Water&nbsp;Damage
+        </p>
+        <img src={logo} className="App-logo-x" alt="logo" />
+        <p className="footer">
+          Copyright © {new Date().getFullYear()} Portland Mac Repair, All rights reserved.
+        </p>
       </header>
     </div>
   );
